580095d59d40db05bdca99b3b5b58dbc63ddf630
Mohandass M <mohandass.mca11@gmail.com>
2022-01-15 20:36:19 +0530

    Added insert files

---
+++ mongo_script_2.js
@@ -0,0 +1,14 @@
+db = conn.getDB("TestDB");
+db.ADM_MENU_SUBMENU.insert(
+{
+  "_id" : ObjectId("5bc6d8c514eda534be1399cd"),
+  "menuId" : 1,
+  "name" : "Orchestrator",
+  "parentCode" : "MENU",
+  "sortOrder" : "1",
+  "type" : "M",
+  "moduleCode" : "M",
+  "subMenu" : false,
+  "url" : "/orchestrator",
+  "subMenuItems" : []
+});
